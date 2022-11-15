e7aa80224b14494ac760ae80a869d470cd2d601d
Atul Y <99254634+ATUL9372@users.noreply.github.com>
2022-11-15 14:32:23 +0530

    solve all error v2.1

--- linter-test.py
+++ linter-test.py
@@ -1,21 +1,34 @@
- import os     # flake8 error in this code not use import os 
+#  import os     # flake8 error in this code not use import os 
   
-  x = 5  # black linter (formatter) error this line 
-        # black linter extra spaces line add
+#   x = 5  # black linter (formatter) error this line 
+#         # black linter extra spaces line add
+# x = 5 # isort error
+# y=10  # pylint space error  
+# temp = x
+
+
+
+
+# x = y
+# y = temp
+# x = y       # isort error multiple time same line use in code 
+# # print('The value of x after swapping: {}'.format(x))
+# # print('The value of y after swapping: {}'.format(y))
+# print('The value of x after swapping: {}'.format(x))
+# print("The value of Y after swapping: {}".format(y)) ## pylint error i'm using Y 
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+#import os     # flake8 error in this code not use import os 
+#  x  = 5  # black linter (formatter) error this line 
+#         # black linter extra spaces line add
 x = 5 # isort error
-y=10  # pylint space error  
+y = 10  # pylint space error  
 temp = x
-
-
-
-
 x = y
 y = temp
-x = y       # isort error multiple time same line use in code 
-# print('The value of x after swapping: {}'.format(x))
-# print('The value of y after swapping: {}'.format(y))
+#x = y       # isort error multiple time same line use in code 
 print('The value of x after swapping: {}'.format(x))
-print("The value of Y after swapping: {}".format(y)) ## pylint error i'm using Y 
-
-
-
+print('The value of y after swapping: {}'.format(y))
+# print('The value of x after swapping: {}'.format(x))
+# print("The value of Y after swapping: {}".format(y)) ## pylint error i'm using Y 
